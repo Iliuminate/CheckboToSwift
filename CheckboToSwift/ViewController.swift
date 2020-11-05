d213f8a98adf8ac81f46f316b56cfd55498e3ffc
Carlos Diaz <carlosdz.centi@gmail.com>
2020-11-05 11:40:53 -0500

    Initial Commit

---
+++ CheckboToSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckboToSwift
+//
+//  Created by Carlos Diaz on 5/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
